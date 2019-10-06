cb900d4409f3b8a96f20f7828a066df26439604c
androidppt1 <walid.bomb@gmail.com>
2019-10-06 13:43:56 +0000

    Add vue-router and delete Helloworld component

--- src/App.vue
+++ src/App.vue
@@ -16,18 +16,17 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    
   },
   data: () => ({
     //
